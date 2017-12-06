8db58e52b1212b54a9770d3abf419fdfb3a2cf2c
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-12-06 01:07:17 +0100

    Use common analysis parts and environment variables

--- ssdeep_analysis_instance.py
+++ ssdeep_analysis_instance.py
@@ -1,7 +1,7 @@
 import sys
 import time
 import logging
-import ssdeep
+import os
 from common_analysis_ssdeep import CommonAnalysisSsdeep
 from mass_api_client import ConnectionManager
 from mass_api_client.resources import Sample, SampleRelationType
@@ -28,25 +28,22 @@ class SsdeepAnalysisInstance:
 
     def __call__(self, scheduled_analysis):
         sample = scheduled_analysis.get_sample()
+        log.info('Analysing {}'.format(sample))
+        report = self.ssdeep_analysis.analyze_string(sample.unique_features['file']['ssdeep_hash'], sample.id)
 
-        similar_samples = []
-        for other in Sample.query(has_file=True):
-            if sample.id == other.id:
-                continue
-            match = ssdeep.compare(sample.unique_features['file']['ssdeep_hash'], other.unique_features['file']['ssdeep_hash'])
-            if match > 0:
-                similar_samples.append((other.id, match))
-
-        for identifier, value in similar_samples:
+        for identifier, value in report['similar samples']:
             self.relation_type.create_relation(sample, Sample.get(identifier), additional_metadata={'match': value})
 
-        scheduled_analysis.create_report(additional_metadata={'number_of_similar_samples': len(similar_samples)})
+        scheduled_analysis.create_report(additional_metadata={'number_of_similar_samples': len(report['similar samples'])})
 
 
 if __name__ == "__main__":
-    ConnectionManager().register_connection('default',
-                                            'IjVhMDBlMTYzMTViNzdmMzE2YTlkYThiZiI.6WyegDGWlqOBKcWlozKJt-2_gKM',
-                                            'http://localhost:8000/api/')
+    api_key = os.getenv('MASS_API_KEY', '')
+    log.info('Got API KEY {}'.format(api_key))
+    server_addr = os.getenv('MASS_SERVER', 'http://localhost:8000/api/')
+    log.info('Connecting to {}'.format(server_addr))
+    timeout = int(os.getenv('MASS_TIMEOUT', '60'))
+    ConnectionManager().register_connection('default', api_key, server_addr, timeout=timeout)
 
     analysis_system_instance = get_or_create_analysis_system_instance(identifier='ssdeep',
                                                                       verbose_name='ssdeep',
